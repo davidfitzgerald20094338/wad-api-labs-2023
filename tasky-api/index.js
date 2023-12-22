c60d5b071df80b8b1eb1ab35860f0af53eb948cd
davidfitzgerald20094338 <74902396+davidfitzgerald20094338@users.noreply.github.com>
2023-12-22 07:46:13 +0000

    Mongo lab Users Router

---
+++ tasky-api/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import tasksRouter from './api/tasks';
+import './db';
+import cors from 'cors';
+import usersRouter from './api/users';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
+
+
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+
+  const errHandler = (err, req, res, next) => {
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if(process.env.NODE_ENV === 'production') {
+      return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+  };
+
+  app.use(errHandler);
+  
+});
